8e16f57199b2260e863d9e43207e360ea1ad350b
Mrrebrik <itibsoft@gmail.com>
2022-06-20 22:03:55 +0700

    - example stage, scene, session - testing async reply to command enter scene and middleware

--- bot.js
+++ bot.js
@@ -1,10 +1,37 @@
 const VkBot = require('node-vk-bot-api');
 const Session = require('node-vk-bot-api/lib/session');
+const Stage = require('node-vk-bot-api/lib/stage');
+const Scene = require('node-vk-bot-api/lib/scene');
+
 
 const token = "vk1.a.RkxqWessN1asBBs449nKg9Y4XGZE4VU71Ykbm0KIcPw9jIosLbMnZAaRjobZkfM0m_Kp4YBAZA7-pA_WoTV2F2YNfICPa5sF-6Ic6N0cq56PGOhtmxQbFDzAECMcGICA5LtG5LQjqO-Up2qzqpR5ZWXLJdd_XQlJSJLntfn5Pz4Q3Wf0OwFnzapCgcsmMZJx";
 const bot = new VkBot(token);
-const session = new Session();
 
+
+
+
+const scene = new Scene('test',
+    (ctx) => {
+        ctx.scene.next();
+        ctx.reply("Тестовое собщение 1");
+
+    },
+    (ctx) => {
+        ctx.scene.leave(); 
+        ctx.reply("Тестовое собщение 1");
+
+    },
+);
+
+const session = new Session();
+const stage = new Stage(scene);
+
+bot.use(session.middleware());
+bot.use(stage.middleware());
+
+bot.command('/test', (ctx) => {
+    ctx.scene.enter('test');
+});
 
 bot.command('/start', async (ctx) => {
     try {
@@ -17,5 +44,6 @@ bot.command('/start', async (ctx) => {
 bot.command('/get-id', async (ctx) =>{
     await ctx.reply(`Ваш индификатор: ${ctx.message.from_id}`);
 });
+
 
 bot.startPolling();
